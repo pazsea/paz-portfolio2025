c74ffa7b1059f88a1974560170356fa13caab949
Patrick Sjöberg <patrick.sjoberg@nordnet.se>
2025-01-05 22:43:34 +0100

    Add close button to mobile and tablet

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useEffect } from "react";
 import CloseIcon from "./icons/CloseIcon";
+import { useGlobal } from "../context/GlobalContext";
 
 interface ModalProps {
   title: string;
@@ -8,6 +9,7 @@ interface ModalProps {
 }
 
 export default function Modal({ title, onClose, children }: ModalProps) {
+  const { isDesktop } = useGlobal();
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
@@ -15,8 +17,14 @@ export default function Modal({ title, onClose, children }: ModalProps) {
       }
     };
 
+    // Prevent background scrolling
+    document.body.style.overflow = "hidden";
+
     document.addEventListener("keydown", handleKeyDown);
-    return () => document.removeEventListener("keydown", handleKeyDown);
+    return () => {
+      document.body.style.overflow = "";
+      document.removeEventListener("keydown", handleKeyDown);
+    };
   }, [onClose]);
 
   return (
@@ -25,18 +33,28 @@ export default function Modal({ title, onClose, children }: ModalProps) {
       onClick={onClose}
     >
       <div
-        className="w-full h-full max-w-lg p-4 rounded-lg bg-background sm:border sm:border-b-color text-text sm:h-auto"
+        className="w-full h-full max-w-lg p-4 rounded-lg bg-background sm:border sm:border-b-color text-text sm:h-auto flex flex-col"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="relative sm:p-4 bg-background text-text">
-          <div className="flex items-baseline justify-between mb-4">
-            <p className="text-xl ">{title}</p>
-            <button aria-label="Close" onClick={onClose}>
-              <CloseIcon />
-            </button>
+        <div className="flex-grow overflow-auto">
+          <div className="relative sm:p-4 bg-background text-text">
+            <div className="flex items-baseline justify-between mb-4">
+              <p className="text-xl ">{title}</p>
+              <button aria-label="Close" onClick={onClose}>
+                <CloseIcon />
+              </button>
+            </div>
+            <div>{children}</div>
           </div>
-          <div>{children}</div>
         </div>
+        {!isDesktop && (
+          <button
+            className="mt-4 w-full py-2 bg-secondary text-white rounded"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        )}
       </div>
     </div>
   );
