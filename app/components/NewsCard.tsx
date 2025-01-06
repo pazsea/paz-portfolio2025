07955e2a892e6fa404299f2cddd9ee188443da68
Patrick Sjöberg <patrick.sjoberg@nordnet.se>
2025-01-06 07:56:25 +0100

    Fix card

--- app/components/NewsCard.tsx
+++ app/components/NewsCard.tsx
@@ -14,7 +14,7 @@ export default function NewsCard({
 }: NewsCardProps) {
   return (
     <div
-      className={`relative bg-background p-2 rounded-lg overflow-hidden max-w-[192px] h-[102px] card`}
+      className={`relative bg-background p-2 rounded-lg overflow-hidden max-w-[192px] min-h-[102px] max-h-[102px] card`}
     >
       {isNew && (
         <div className="absolute top-2 right-2">
@@ -22,7 +22,7 @@ export default function NewsCard({
         </div>
       )}
       <div className="flex flex-col content-between w-full h-full">
-        <h4 className={`p-0 m-0 font-bold line-clamp-2 ${isNew ? "pr-8" : ""}`}>
+        <h4 className={`p-0 m-0 font-bold line-clamp-2 ${isNew ? "pr-2" : ""}`}>
           {title}
         </h4>
         <p className="text-sm line-clamp-2">{description}</p>
